50da911cdaad5b90468c529c0f7b1c1a482e77e9
Hernán Cervera <hjcerveram@hotmail.com>
2021-10-05 21:39:48 -0500

    Initial commit

---
+++ src/main/java/hercerm/uady/fmtreviewsback/FmtReviewsBackApplication.java
@@ -0,0 +1,13 @@
+package hercerm.uady.fmtreviewsback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmtReviewsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmtReviewsBackApplication.class, args);
+	}
+
+}
